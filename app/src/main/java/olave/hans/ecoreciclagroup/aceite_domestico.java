bd3c906aabe0bf060acc008141a4d5928909d15b
Future000-1 <villamarinval@gmail.com>
2023-11-09 09:39:32 -0500

    funcion app

--- app/src/main/java/olave/hans/ecoreciclagroup/aceite_domestico.java
+++ app/src/main/java/olave/hans/ecoreciclagroup/aceite_domestico.java
@@ -2,10 +2,16 @@ package olave.hans.ecoreciclagroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class aceite_domestico extends AppCompatActivity {
 
@@ -17,6 +23,15 @@ public class aceite_domestico extends AppCompatActivity {
 
     ImageView estadisticas_2;
 
+    Button registrarcantidad;
+
+    EditText numcantidad;
+    EditText acumagua;
+    private int resultadoacumulado=0;
+
+    private  int resultadototal=0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +42,9 @@ public class aceite_domestico extends AppCompatActivity {
         sabias_2= findViewById(R.id.sabias_2);
         ubi_2= findViewById(R.id.ubi_2);
         estadisticas_2= findViewById(R.id.estadisticas_2);
-
+        numcantidad= findViewById(R.id.Ingresa_la_cantidad);
+        acumagua=findViewById(R.id.acumAgua);
+        registrarcantidad= findViewById(R.id.ingresocantidad);
 
         Intent regresoHome= new Intent(getApplicationContext(),
                 Home.class);
@@ -86,6 +103,45 @@ public class aceite_domestico extends AppCompatActivity {
                 startActivity(regresoestadistica);
             }
         });
+
+
+
+
+
+        //FUNCION DE LA APP
+
+
+        //ACUMULADO ACEITE
+
+        registrarcantidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        //ACUMULADO AGUA
+
+        ArrayList <Integer> cantidadagua= new ArrayList<>();
+        registrarcantidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String valoringresado= numcantidad.getText().toString();
+                  //  int valor= Integer.parseInt(valoringresado);
+                    int valor = Integer.parseInt(numcantidad.getText().toString());
+                    resultadoacumulado=   valor * 40000;
+                    resultadototal= resultadoacumulado + resultadototal;
+                    cantidadagua.add(resultadoacumulado);
+                    acumagua.setText(String.valueOf(resultadototal));
+
+
+                }
+        });
+
+        resultadoacumulado = 0;
+
+        //ACUMULADO JABON
 
     }
 }
